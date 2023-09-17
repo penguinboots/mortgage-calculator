502d91dee35e714409dcd54a4716cfe71adc0904
penguinboots <sabrina.ynw@gmail.com>
2023-09-17 11:29:21 -0700

    add error handling

--- src/Main.java
+++ src/Main.java
@@ -8,19 +8,44 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal;
+        double monthlyRate;
+        int payments;
 
-        System.out.print("Annual Interest Rate: ");
-        double annualRate = scanner.nextDouble();
-        double monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal <= 0) {
+                System.out.println("Enter a valid amount.");
+                continue;
+            }
+            break;
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int payments = years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            double annualRate = scanner.nextDouble();
+            if (annualRate <= 0) {
+                System.out.println("Enter a valid rate.");
+                continue;
+            }
+            monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
+            break;
+        }
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            byte years = scanner.nextByte();
+            if (years < 1 || years > 40) {
+                System.out.println("Enter a valid period.");
+                continue;
+            }
+            payments = years * MONTHS_IN_YEAR;
+            break;
+        }
 
         double mathPower = Math.pow(1 + monthlyRate, payments);
-        double mortgage = principal * ( monthlyRate * mathPower  / (mathPower - 1) );
+        double mortgage = principal * (monthlyRate * mathPower / (mathPower - 1));
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.print("Monthly payment: " + formattedMortgage);
 
