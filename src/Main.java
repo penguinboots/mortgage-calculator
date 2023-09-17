ab2b6fa693f99b0eb619e65c009cb99bc67a412e
penguinboots <sabrina.ynw@gmail.com>
2023-09-17 11:48:03 -0700

    extract method

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         Scanner scanner = new Scanner(System.in);
 
         int principal;
-        double monthlyRate;
-        int payments;
+        double annualRate;
+        byte years;
 
         while (true) {
             System.out.print("Principal: ");
             principal = scanner.nextInt();
-            if (principal <= 0) {
-                System.out.println("Enter a valid amount.");
-                continue;
-            }
-            break;
+            if (principal > 0) break;
+            System.out.println("Enter a valid amount.");
         }
 
         while (true) {
             System.out.print("Annual Interest Rate: ");
-            double annualRate = scanner.nextDouble();
-            if (annualRate <= 0) {
-                System.out.println("Enter a valid rate.");
-                continue;
-            }
-            monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
-            break;
+            annualRate = scanner.nextDouble();
+            if (annualRate > 0) break;
+            System.out.println("Enter a valid rate.");
         }
 
         while (true) {
             System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
-            if (years < 1 || years > 40) {
-                System.out.println("Enter a valid period.");
-                continue;
-            }
-            payments = years * MONTHS_IN_YEAR;
-            break;
+            years = scanner.nextByte();
+            if (years > 1 && years < 40) break;
+            System.out.println("Enter a valid period.");
         }
 
-        double mathPower = Math.pow(1 + monthlyRate, payments);
-        double mortgage = principal * (monthlyRate * mathPower / (mathPower - 1));
+        double mortgage = calculateMortgage(principal, annualRate, years);
+
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.print("Monthly payment: " + formattedMortgage);
+    }
 
+    public static double calculateMortgage(int principal, double annualRate, int years) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        double monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
+        int payments = years * MONTHS_IN_YEAR;
+
+        double mathPower = Math.pow(1 + monthlyRate, payments);
+
+        return principal * (monthlyRate * mathPower / (mathPower - 1));
     }
 }
