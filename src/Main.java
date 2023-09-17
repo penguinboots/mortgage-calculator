6daa83d127a373cfb971a5aaaf4e776352f27f2b
penguinboots <sabrina.ynw@gmail.com>
2023-09-17 12:31:50 -0700

    add balance calculator

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,30 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    static final byte MONTHS_IN_YEAR = 12;
+    static final byte PERCENT = 100;
+
     public static void main(String[] args) {
         int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
         double annualRate = readNumber("Annual Rate: ", 1, 50);
-        byte years = (byte) readNumber("Period (Years) :", 1, 50);
+        byte years = (byte) readNumber("Period (Years): ", 1, 50);
 
         double mortgage = calculateMortgage(principal, annualRate, years);
-
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.print("Monthly payment: " + formattedMortgage);
+        System.out.println("Monthly payment: " + formattedMortgage);
+
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("--------");
+        System.out.println("\nBalance at:");
+        for (short month = 0; month <= years * MONTHS_IN_YEAR; month+=12) {
+            double balance = calculateBalance(principal, annualRate, years, month);
+            System.out.println(month/MONTHS_IN_YEAR + " year(s): " + NumberFormat.getCurrencyInstance().format(balance));
+        }
     }
 
     public static double calculateMortgage(int principal, double annualRate, int years) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         double monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
         int payments = years * MONTHS_IN_YEAR;
 
@@ -35,5 +44,14 @@ public class Main {
             System.out.println("Enter a value between " + min + " and " + max + ".");
         }
         return value;
+    }
+
+    public static double calculateBalance(int principal, double annualRate, byte years, short paymentsMade) {
+        int payments = years * MONTHS_IN_YEAR;
+        double monthlyRate = annualRate / MONTHS_IN_YEAR / PERCENT;
+
+        return principal
+                * (Math.pow(1 + monthlyRate, payments) - Math.pow(1 + monthlyRate, paymentsMade))
+                / (Math.pow(1 + monthlyRate, payments) - 1);
     }
 }
